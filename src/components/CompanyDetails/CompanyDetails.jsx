8856ffc33e425ce4c3f1f05543a2583c436dbce8
Bhoomika <bhoomikahm18@gmail.com>
2024-08-02 16:11:00 +0530

    chnaging font style and family

--- src/components/CompanyDetails/CompanyDetails.jsx
+++ src/components/CompanyDetails/CompanyDetails.jsx
@@ -4,8 +4,9 @@ import './CompanyProfile.css';
 function CompanyDetails() {
     return (
         <div className='profile'>
+            {/* <h1>Welcome to company</h1> */}
             <img className='img-1' src='https://www.slideegg.com/image/multi_slide/2571/50595-Company-Profile-PPT_01.PNG' />
-            <h1 className='heading-1'>Technology is the application of conceptual knowledge to achieve practical goals, especially in a reproducible way.</h1>
+            <h2 className='heading-1'>Technology is the application of conceptual knowledge to achieve practical goals, especially in a reproducible way.</h2>
             <hr />
             <h1 className='heading-2'>Work Toward Your Goals</h1>
             <img className='img-2' src='https://cms-article.forbesindia.com/media/images/2023/Sep/img_218241_workfromhome.jpg' />
